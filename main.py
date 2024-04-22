23ebe2e5780d57387073f4766044d0d861e614c0
Meesen11 <157275588+Meesen11@users.noreply.github.com>
2024-04-22 15:40:28 +0300

    end obnova

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-
 import telebot
 from telebot import types
 
 bot = telebot.TeleBot('6392514625:AAFymeOXq450T04_m-prC-D5VYTQA2dBNGE')
 
+balans = 0
+ref = 0
+crypto_wallet = []
+referral_links = {}
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -14,24 +17,31 @@ def start(message):
     if member.status == 'member' or member.status == 'creator':
         reply_markup = types.ReplyKeyboardMarkup()
 
-        btn1 = types.KeyboardButton(r'Условия\Terms 📃')
-        btn2 = types.KeyboardButton(r'Кошелек\Wallet 👛')
-        btn3 = types.KeyboardButton(r'Баланс\Balance 💰')
-        btn4 = types.KeyboardButton(r'Пригласить/Invite 🫂')
+        btn1 = types.KeyboardButton(r'Профиль')
+        btn2 = types.KeyboardButton(r'Криптокошелек')
+        btn3 = types.KeyboardButton(r'Реферальная система')
 
         reply_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         reply_markup.add(btn1, btn2)
-        reply_markup.add(btn3, btn4)
+        reply_markup.add(btn3)
 
-        bot.send_message(message.chat.id, 'Выберите действие:', reply_markup=reply_markup)
-    
         inline_markup = types.InlineKeyboardMarkup()
         inline_markup.add(types.InlineKeyboardButton('Реферал', callback_data='invite')) 
-        bot.send_message(message.chat.id, 'AIRDROP GUMMY BEAR COIN 🧸 Мы платим целых 250 $GMBR за одного приведенного друга Это самые лучшие условия для AIRDROP !', reply_markup=inline_markup)
+        bot.send_message(message.chat.id, 'AIRDROP QWETON COIN 🧸 Мы платим целых 250 $QWT за одного приведенного друга Это самые лучшие условия для AIRDROP !', reply_markup=inline_markup)
     
     if member.status != 'member' and member.status != 'creator':
         inline_markup = types.InlineKeyboardMarkup()
         inline_markup.add(types.InlineKeyboardButton('Join QWETON', url='https://t.me/+OsklCmoqvCRkOWJi'))
         bot.send_message(message.chat.id, 'Чтобы участвовать в AIRDROP, Вам необходимо сначала подписаться на следующие каналы 👇', reply_markup=inline_markup)
+
+
+
+
+
+
+@bot.message_handler(content_types=['text'])
+def profil(message):
+    if message.text.lower() == 'профиль':
+        bot.send_message(message.chat.id, f'Информация профиля: \n Баланc: {balans} $QWT \n Количество рефералов: {ref}')      
 
 bot.polling()
--- profil.py
+++
@@ -1,7 +0,0 @@
-import telebot
-from telebot import types
-
-
-@bot.message_handler(func=lambda message: message.text.lower() == 'Профиль')
-def profil(message):
-    bot.reply_to(message, f'Ты пригласил: \n Твой баланс -  QWT')
